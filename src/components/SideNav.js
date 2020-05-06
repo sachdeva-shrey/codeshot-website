f90eab1fe746d79f70e588d29786092988c0d94d
Shrey Sachdeva <shreysachdeva.2000@gmail.com>
2020-05-06 23:28:34 +0530

    style(SideNav): add logo
    Signed-off-by: Shrey Sachdeva <shreysachdeva.2000@gmail.com>

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -1,17 +1,29 @@
 import React from "react";
 import "semantic-ui-css/semantic.css";
+import styled from "styled-components";
 
 import DropdownWrapper from "./Dropdown";
 import "../lib/styles/dropdownStyles.css";
 import Buttons from "./Button";
+import logo from "../lib/icons/logo.png";
 
-function SideNav ({ takeSnap }) {
+function SideNav({ takeSnap }) {
   return (
     <>
+      <Logo src={logo} alt="logo" />
       <DropdownWrapper />
       <Buttons takeSnap={takeSnap} />
     </>
   );
-};
+}
+
+const Logo = styled.img`
+  position: fixed;
+  top: 20px;
+  left: 7%;
+  z-index: 100;
+  height: 30px;
+  width: auto;
+`;
 
 export default SideNav;
